16cb3e7e43fa572aae4bfd50ed81a9e38b3e1b28
mukker-tanay <82504598+mukker-tanay@users.noreply.github.com>
2026-01-04 02:22:56 +0530

    proxy changes

--- FrontEnd/next.config.js
+++ FrontEnd/next.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: [
-    'http://192.168.0.8:3000'
-  ],
+  // Rewrites are no longer needed as Nginx handles the proxying
+  /*
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/:path*', // Proxy to Backend
+      },
+      {
+        source: '/docs',
+        destination: 'http://127.0.0.1:8000/docs', // Proxy Swagger UI
+      },
+      {
+        source: '/openapi.json',
+        destination: 'http://127.0.0.1:8000/openapi.json', // Proxy OpenAPI schema
+      },
+    ]
+  },
+  */
 };
 
 module.exports = nextConfig;
