a383ee2f7aa5fa5846d7c0d04fb15b28179f3fe5
mukker-tanay <82504598+mukker-tanay@users.noreply.github.com>
2026-06-14 18:46:41 +0530

    nextjs fixes

--- FrontEnd/next.config.js
+++ FrontEnd/next.config.js
@@ -1,7 +1,24 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8080/:path*',
+      },
+      {
+        source: '/docs',
+        destination: 'http://127.0.0.1:8080/docs',
+      },
+      {
+        source: '/openapi.json',
+        destination: 'http://127.0.0.1:8080/openapi.json',
+      },
+    ];
+  },
+};
 
 module.exports = withSentryConfig(nextConfig, {
   silent: true,
